31cc38d5c9d0e78685470dab56b9ed6b612cc70d
irfan-ansari-au28 <irfan398649@gmail.com>
2022-04-08 01:14:37 +0530

    Implemented tab bar navigator

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,23 +1,13 @@
 import React from "react";
 import { Searchbar } from "react-native-paper";
-import { StatusBar, SafeAreaView } from "react-native";
+import { FlatList } from "react-native";
 import styled from "styled-components/native";
 
+import { SafeArea } from "../../../components/utility/safe-area.component";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
-import { FlatList } from "react-native";
 import { Spacer } from "../../../components/spacer/spacer.component";
 
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
-`;
-
 const SearchContainer = styled.View`
-  padding: ${(props) => props.theme.space[3]};
-`;
-
-const RestaurantListContainer = styled.View`
-  flex: 1;
   padding: ${(props) => props.theme.space[3]};
 `;
 
@@ -30,7 +20,7 @@ const RestaurantList = styled(FlatList).attrs({
 export const RestaurantsScreen = () => (
   <SafeArea>
     <SearchContainer>
-      <Searchbar placeholder="Search" />
+      <Searchbar />
     </SearchContainer>
     <RestaurantList
       data={[
